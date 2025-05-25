51f681afab8ae5d962d50302bbbd61276c199e0d
joel odunoye <joelodunoye@gmail.com>
2025-05-25 09:27:49 +0100

    Add basic arithmetic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# Basic Operations 
+
+number1 = 10
+number2 = 5 
+
+# Perform arithmetic operations
+
+addition = number1 + number2 # this will add the two numbers 
+subtraction = number1 - number2 # this will subtract the second number from the first
+multiplication = number1 * number2 # this will multiply the two numbers
+
+#print the results
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(addition))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(subtraction))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(multiplication))
