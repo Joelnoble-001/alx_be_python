e7fc93984d6bdbec0f9db71a418de883eca2456d
joel odunoye <joelodunoye@gmail.com>
2025-05-28 08:19:53 +0100

    add multiplication table

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,10 @@
+# multiplication table
+
+# define the variable 
+
+number = int(input("Enter a number to see its multiplication table:")) # prompt the user for a number
+
+# print the multiplication table using a for loop
+for y in range(1, 11):
+    z = number * y
+    print(f"{number} x {y} = {z}")
