0f51754ecd905b9d0c7aae52967c4321174b8abe
joel odunoye <joelodunoye@gmail.com>
2025-05-28 08:02:23 +0100

    Update match case calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,36 +4,33 @@
 
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-
-while True:
-    operation_type = input("Choose the operation (+, -, *, /): ").strip()
+operation = input("Choose the operation (+, -, *, /): ").strip()
 
     # Match case to perform the operation
 
-    match operation_type:
-        case "+":
+match operation:
+    case "+":
             result = num1 + num2
             print(f"The result is {result}")
-            break  # Exit the loop after a successful operation
-        case "-":
+    case "-":
             result = num1 - num2
             print(f"The result is {result}")
-            break  # Exit the loop after a successful operation
-        case "*":
+    case "*":
             result = num1 * num2
             print(f"The result is {result}")
-            break  # Exit the loop after a successful operation
-        case "/":
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+            print(f"The result is {result}")
+        else:
+            print("Error: Division by zero is not allowed.")
+            num2 = int(input("Enter the second number: "))
             if num2 != 0:
                 result = num1 / num2
                 print(f"The result is {result}")
             else:
-                print("Error: Division by zero is not allowed.")
-                num2 = int(input("Enter the second number: "))
-                result = num1 / num2
-                print(f"The resuslt is {result}")
-            break  # Exit the loop after a successful operation
-        case _:
-            print("Invalid operation. Choose from +, -, *, /.")
+                print("Error: Division by zero is still not allowed.")
+    case _:
+            print("Invalid operation. Please choose from +, -, *, /.")
 
     # End of match case calculator
