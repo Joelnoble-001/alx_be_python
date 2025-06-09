bcbe14f03e608523cd99d37b1c494ea601af59ad
joel odunoye <joelodunoye@gmail.com>
2025-06-09 09:16:51 +0100

    update temperature conversion tool script

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,8 @@
 # temp_conversion_tool.py
 
 # Define Global Conversion Factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 # Implement Conversion Functions
 def convert_to_celsius(fahrenheit):
